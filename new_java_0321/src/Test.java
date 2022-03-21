e6b8c427c3c9dd4259706c3aa6984d5ecb02c171
Byrant-liu11 <lk5425864@163.com>
2022-03-21 21:55:45 +0800

    JAVA初识

---
+++ new_java_0321/src/Test.java
@@ -0,0 +1,9 @@
+public class Test {
+    public static void main(String[] args) {
+//        System.out.println("hello world");
+        int num = 10;
+        //int为变量类型，num为变量名，10为初始值
+        num = 20; //修改变量值
+        System.out.println(num);
+    }
+}
