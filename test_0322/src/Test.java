bc94910b925d3533a3437b6ca515bacc5ebda0b5
Byrant-liu11 <lk5425864@163.com>
2022-03-22 22:01:23 +0800

    变量和数据类型1

---
+++ test_0322/src/Test.java
@@ -0,0 +1,77 @@
+public class Test {
+    public static void main(String[] args){
+//        System.out.println("hello world");
+        //int
+        //Integer 称为包装类
+        //比如：
+        //Integer:int的包装类
+        //Long:long的包装类
+        //Short:short的包装类
+        //Double:double的包装类
+//        System.out.println(Integer.MAX_VALUE);//int最大值
+//        System.out.println(Integer.MIN_VALUE);
+//        System.out.println(Integer.MAX_VALUE + 1);//会出现内存溢出，结果不对
+//        System.out.println(Long.MAX_VALUE);
+//        System.out.println(Long.MIN_VALUE);
+//        long num = 10;
+//        double a = 1.1;
+//        System.out.println(a * a);
+//        if(a*a == 1.21){
+//            System.out.println("相等");
+//        }else{
+//            System.out.println("不相等");
+//        }
+//        if (a * a - 1.21 <0.000000001
+//                && a * a - 1.21 > -0.000000001){
+//            System.out.println("相等");
+//        }else{
+//            System.out.println("不相等");
+//        }
+//        char a = 'A';
+//        char b = '中';
+//        System.out.println(b);
+//        boolean a = true;
+//        String str = "hello";
+//        String str = "my name is \"liu\"";
+//        System.out.println(str);
+//        System.out.println(str.length());//求字符串长度
+        //字符串拼接
+//        String a = "hello";
+//        String b = " world";
+//        System.out.println(a + b);
+        //变量命名软性规则
+        //小驼峰（首个单词首字母小写，后续单词首字母大写）  --变量、方法
+        //studentName    studentNameLength
+        //大驼峰（所有单词首字母都大写）    --类名
+        //StudentName
+//        int 整数 = 10;//合法，但是不用
+        //final int num = 10;  //final修饰num不可修改，final相当于C语言中的const
+//        int a = 10;
+//        long b = 20;
+//        b = a;
+        //代码中一旦出现强制类型转换
+        //代码的正确性需要程序员自己进行确定
+//        a = (int)b;//强制类型转换
+//        byte a = 10;
+//        byte b = 20;
+//        byte c = (byte) (a + b);  //出错后采用alt+enter可以自动修复，进行强制类型转换
+//      整数转字符串
+//        int num = 10;
+//        String str = String.valueOf(num);
+//      字符串转整数
+//        String str = "100";
+        //int num = Integer.parseInt(str);//尽量用这种
+        //int num = Integer.valueOf(str);//该方式会有差别
+        //异常指的是运行时的错误
+        //编译期的错误往往是语法上错误
+//        int num = 10;
+//        int ret = ++num;
+//        System.out.println(ret);//先加，后赋值
+//        int a = 10;
+//        int sul = a++;
+//        System.out.println(sul);//先赋值，后加
+        //Java中前置++与后置++区别不大
+        //Java中习惯于写后置++
+
+    }
+}
